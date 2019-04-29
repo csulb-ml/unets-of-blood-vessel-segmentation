5254f43b88ead054c15ab7e21ce19f213f4597d8
vinchg <vincentkcheong@gmail.com>
2019-04-29 12:59:50 -0700

    Fixes to code

--- sdcnn/sdcnn.py
+++ sdcnn/sdcnn.py
@@ -2,8 +2,8 @@ from keras.models import Model
 from keras.layers import Input, Conv2D, Reshape, core, Dropout, Add, SeparableConv2D, BatchNormalization
 from keras.optimizers import Adam
 
-# Smoothed Dilated CNN (234) with Shared and Separable Convolution (SSC) included
-def sdcnn(patch_height, patch_width, n_ch):
+# Dilated CNN (234) with Shared and Separable Convolution (SSC) included
+def ss_dcnn(patch_height, patch_width, n_ch):
     print('********************SS_DCNN********************')
     inputs = Input(shape=(patch_height, patch_width, n_ch))
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_last')(inputs)
@@ -30,26 +30,26 @@ def sdcnn(patch_height, patch_width, n_ch):
 def dilation_block(_inputs, _filters, _kernel_size, _strides=1, _dilation_rate=1, ssc=False):
     if ssc == True:
         block_1 = SeparableConv2D(_filters, _kernel_size, activation='relu', padding='same', data_format='channels_last', depth_multiplier=1, use_bias=False)(_inputs)
-        block_1 = BatchNormalization()(block_1)
-        block_1 = Conv2D(_filters, _kernel_size, activation='relu', padding='same', data_format='channels_last', dilation_rate=_dilation_rate)(block_1)
     else:
         block_1 = Conv2D(_filters, _kernel_size, activation='relu', padding='same', data_format='channels_last', dilation_rate=_dilation_rate)(_inputs)
     block_1 = BatchNormalization()(block_1)
     if ssc == True:
+        block_1 = SeparableConv2D(_filters, _kernel_size, activation='relu', padding='same', data_format='channels_last', dilation_rate=_dilation_rate)(block_1)
+    else:
         block_1 = Conv2D(_filters, _kernel_size, activation='relu', padding='same', data_format='channels_last', dilation_rate=_dilation_rate)(block_1)
-    block_1 = Conv2D(_filters, _kernel_size, activation='relu', padding='same', data_format='channels_last', dilation_rate=_dilation_rate)(block_1)
     block_1 = BatchNormalization()(block_1)
     
     merge_1 = Add()([_inputs, block_1])
     
     if ssc == True:
         block_2 = SeparableConv2D(_filters, _kernel_size, activation='relu', padding='same', data_format='channels_last', depth_multiplier=1, use_bias=False)(merge_1)
-        block_1 = BatchNormalization()(block_1)
-    block_2 = Conv2D(_filters, _kernel_size, activation='relu', padding='same', data_format='channels_last', dilation_rate=_dilation_rate)(merge_1)
+    else:
+        block_2 = Conv2D(_filters, _kernel_size, activation='relu', padding='same', data_format='channels_last', dilation_rate=_dilation_rate)(merge_1)
     block_2 = BatchNormalization()(block_2)
     if ssc == True:
         block_2 = SeparableConv2D(_filters, _kernel_size, activation='relu', padding='same', data_format='channels_last', depth_multiplier=1, use_bias=False)(block_2)
-    block_2 = Conv2D(_filters, _kernel_size, activation='relu', padding='same', data_format='channels_last', dilation_rate=_dilation_rate)(block_2)
+    else:
+        block_2 = Conv2D(_filters, _kernel_size, activation='relu', padding='same', data_format='channels_last', dilation_rate=_dilation_rate)(block_2)
     block_2 = BatchNormalization()(block_2)
     
     merge_2 = Add()([merge_1, block_2])
